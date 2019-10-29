612bc942f0b406c547ddcecc922c710e92ca4c1e
Minnowfeather <Minnowfeather@github.com>
2019-10-28 19:11:42 -0700

    Added help and list commands

--- bot.js
+++ bot.js
@@ -18,23 +18,35 @@ client.on('ready', () => {
 
 client.on('message', msg => {
 	if (!msg.author.bot) {
-		var s = msg.content;
-		var f = "";
-		if(s.charAt(0) == ","){
-			for(var i = 1; i < s.length; i++){
-				f += s.charAt(i);
-			}
-			var path = "Images/" + f +".png";
-			try{
-				if(fs.existsSync(path)){
-					attachment = new Attachment(path);
-					msg.channel.send(attachment);
-				} else{
+		if(msg.content == ",help"){
+			msg.channel.send("To use this bot: \nType a comma followed by the filename of the reaction img you want. \nEx: `,josh` produces the josh image\nFor a list of all images, type `,list`");
+		}else if(msg.content == ",list"){
+			fs.readdir("Images/", function(err, items){
+				var files = "";
+				for(var i = 0; i < items.length; i++){
+					files = files + items[i] + "\n";
+				}
+				msg.channel.send("__Here are all the reaction images available:__\n" + files);	
+			});
+		} else{
+			var s = msg.content;
+			var f = "";
+			if(s.charAt(0) == ","){
+				for(var i = 1; i < s.length; i++){
+					f += s.charAt(i);
+				}
+				var path = "Images/" + f +".png";
+				try{
+					if(fs.existsSync(path)){
+						attachment = new Attachment(path);
+						msg.channel.send(attachment);
+					} else{
+						msg.channel.send("Image not found!");
+					}
+				} catch(err){
+					console.error(err);
 					msg.channel.send("Image not found!");
 				}
-			} catch(err){
-				console.error(err);
-				msg.channel.send("Image not found!");
 			}
 		}	
 	}
